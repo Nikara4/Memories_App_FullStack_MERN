6f249f0044145786b284bf4a64b218c17a230d94
Aleksandra <olaszczur93@gmail.com>
2022-03-01 20:01:39 +0100

    create Home/NavBar/Auth components and routing

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,6 @@
+const Auth = () => {
+  const state = null;
+  return <h1>Auth</h1>;
+};
+
+export default Auth;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,5 @@
 export { default as Posts } from "./Posts/Posts";
 export { default as Form } from "./Form/Form";
+export { default as NavBar } from "./NavBar/NavBar";
+export { default as Home } from "./Home/Home";
+export { default as Auth } from "./Auth/Auth";
